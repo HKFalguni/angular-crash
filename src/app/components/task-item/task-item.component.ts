c867a2fac7b05324e09770e1e139755bc5b19cc3
HKFalguni <hkfalguni@gmail.com>
2021-12-06 17:07:09 +0530

    Angular-Crash Course

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+
+  @Input() task!:Task;
+  @Output() onDeleteTask:EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder:EventEmitter<Task> = new EventEmitter();
+  faTimes=faTimes;
+  constructor() { }
+
+  ngOnInit() {
+  }
+onDelete(task: any){
+  this.onDeleteTask.emit();
+}
+onToggle(task: any){
+  this.onToggleReminder.emit();
+}
+}
